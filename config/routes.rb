42f2fca8f3374b87676fd6b38d6aaa03769907df
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-03-31 13:14:34 -0400

    Update route and application view per devise install

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get "posts/index"
-  get "posts/new"
-  get "posts/create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,6 +9,6 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root "posts#index"
+  resources :posts, only: [ :new, :index, :create ]
 end
